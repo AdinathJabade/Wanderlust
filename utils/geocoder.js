61f40efc66a164ac8e47ef58964a14da9f95ec50
Adinath_Jabade <adijabade5@gmail.com>
2025-09-04 10:18:47 +0530

    header include of openstreetmap

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -1,7 +1,10 @@
 const NodeGeocoder = require('node-geocoder');
 
+
 const options = {
   provider: 'openstreetmap',
+  httpAdapter: 'https',
+  userAgent: 'TripsterApp/1.0 (adujabade@gmail.com)',
 };
 
 const geocoder = NodeGeocoder(options);
